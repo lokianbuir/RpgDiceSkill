26852a28c2fe220f88eb410ef873003264544907
John McCandless <john.mccandless@q2ebanking.com>
2018-10-30 15:56:34 -0500

    test commit, altered spacing

--- RpgDice_py3.py
+++ RpgDice_py3.py
@@ -1,9 +1,9 @@
 from random import randint
 
 count = 1  # for tracking dice rolled
+advRoll = input("Is this roll with advantage (a) or disadvantage (d), or neither (n)?")
 userInput = input("How many and what dice are you rolling? (Use standard RPG dice terms: 3d6, 1d12, etc.:")
 rollBonus = input("Is there a bonus to the roll, if yes how much? If no, put 0:")
-advRoll = input("Is this roll with advantage (a) or disadvantage (d), or neither (n)?")
 diceTotal = 0  # No dice have been rolled yet, duh...
 
 results = userInput.split("d")
@@ -13,22 +13,19 @@ results = [int(i) for i in results]
 
 diceNumber = results[0]
 diceSides = results[1]
-
-
 # print(diceNumber)
 # print(diceSides)
 
-class StandardRollLoop:
-    while True:
-        diceSides = results[1]
-        diceSides = randint(1, diceSides)
+while True:
+    diceSides = results[1]
+    diceSides = randint(1, diceSides)
 
-        diceTotal = diceTotal + diceSides
-        print("Die", count, "rolled", diceSides)
-        count += 1
+    diceTotal = diceTotal + diceSides
+    print("Die", count, "rolled", diceSides)
+    count += 1
 
-        if count >= diceNumber + 1:
-            rollBonus = diceTotal + int(rollBonus)
-            print("Your total rolled is:", diceTotal)
-            print("With the bonus:", rollBonus)
-            break
+    if count >= diceNumber + 1:
+        rollBonus = diceTotal + int(rollBonus)
+        print("Your total rolled is:", diceTotal)
+        print("With the bonus:", rollBonus)
+        break
